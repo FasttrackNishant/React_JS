b1eaf66dc5a665983cdf553bbce6adc55f3ce825
Nishant <ncp1435@gmail.com>
2022-09-01 19:53:18 +0530

    text Utils created

--- Ultlimate_React/my-app/src/App.js
+++ Ultlimate_React/my-app/src/App.js
@@ -1,36 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './componets/Navbar';
+import Textform from './componets/Textform';
+import About from './componets/About';
+import React, { useState } from 'react';
+import Alert from './componets/Alert';
+
 
 let name = "javascript"
 function App() {
+  //this variable states whether dark mode is enabled or not
+
+  const [mode, setmode] = useState('light');
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+
+    setAlert(
+      {
+        msg: message,
+        type: type
+      }
+    )
+
+  }
+
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+      document.body.style.backgroundColor = "#042743"
+      console.log('dark set');
+      showAlert("Dark mode has been set", "success ")
+
+    }
+    else {
+      setmode('light')
+      document.body.style.backgroundColor = "white"
+      console.log('light set');
+
+    }
+  }
+
   return (
     <>
 
-      <nav className="navbar navbar-expand-lg  bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUtils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active " aria-current="page" href="/">Home</a>
-              </li>
-             
-              <li className="nav-item">
-                <a className="nav-link active " aria-current="page" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container py-5">
+        <Textform showAlert = {showAlert}  heading="Enter your text here" title="Analyze your Text here " mode={mode}  />
+      </div>
+      <About />
+
     </>
   );
 }
+
 
 export default App;
